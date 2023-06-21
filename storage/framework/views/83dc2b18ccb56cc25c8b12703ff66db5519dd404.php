535e96ff99b791c5cd702cbb8cebdba093118fb7
hafizen <kurniahafizwan@gmail.com>
2023-06-21 11:03:52 +0700

    commit

---
+++ storage/framework/views/83dc2b18ccb56cc25c8b12703ff66db5519dd404.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+<!-- Log on to codeastro.com for more projects! -->
+     ©  <?php echo e(date('Y')); ?> Simple Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Ali Aqa Atayee</span>.
+</footer><?php /**PATH C:\xampp\htdocs\AttendanceMS-Laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
