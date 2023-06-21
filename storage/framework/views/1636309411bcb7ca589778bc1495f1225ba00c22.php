30e02a2b929b89bd15b983cf7e7c489fbac377aa
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-06-21 11:53:46 +0700

    Delete storage directory

--- storage/framework/views/1636309411bcb7ca589778bc1495f1225ba00c22.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-     ©  <?php echo e(date('Y')); ?> Simple Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Ali Aqa Atayee</span>.
-</footer><?php /**PATH D:\XAMPP\htdocs\AttendanceMS-Laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
